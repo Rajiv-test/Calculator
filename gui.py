7eecfce96bfad2759357c405d2ef73ddd2a9edaf
Rajiv-test <tathireddyrajivreddy@gmail.com>
2025-03-01 14:38:07 +0530

    first commit

---
+++ gui.py
@@ -0,0 +1,8 @@
+from tkinter import *
+from tkinter import ttk
+
+class Calculator(Tk):
+    def __init__(self):
+        super().__init__()
+        self.title("Calculator")
+        self.mainloop()
